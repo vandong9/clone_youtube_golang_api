2f90055c3b30adf722395ff59d5ca7267b6fcefd
dongha <vandong9@gmail.com>
2023-02-28 01:09:27 +0700

    update

--- common/constant/constant.go
+++ common/constant/constant.go
@@ -3,5 +3,6 @@ package constant
 const (
 	Storage_record_not_found           = "Storage_record_not_found_404"
 	Storage_update_record_fail         = "Storage_update_record_fail"
-	Max_Channel_Page_Size      float64 = 50
+	Default_Channel_Page_Size  float64 = 20
+	Max_Channel_Page_Size      float64 = 200
 )
--- modules/video/controller/query/entity.go
+++ modules/video/controller/query/entity.go
@@ -1,4 +1,7 @@
 package query
 
 type QueryInput struct {
+	ChannelID string `json:"channel_id"`
+	PageIndex uint   `json:"page_index"`
+	PageSize  uint   `json:"page_size"`
 }
